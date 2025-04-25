390d39803dda20e15d78a356d302f102e0ee16ce
Melissa Cruz <melissa.e.cruz@gmail.com>
2025-04-25 19:53:14 -0400

    including the feedback from kit and formatting

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -1,35 +1,52 @@
-function Action(){
-    return (
-        <main>
-            <div className="main-div-action-h1">
-                <h1>What action would you like to take?</h1>
-            </div>
-            <form className="main-action-form">   
-                <div>
-                    <input type="radio" id="action_upload_progress" name="action_type" value="ActionUploadProgress"/>
-                    <label> Upload progress, feelings, or blocker for your vision.</label>
-                </div>
-                <br/>
+function Action() {
+  return (
+    <main>
+      <div className="main-div-action-h1">
+        <h1>What action would you like to take?</h1>
+      </div>
+      <form className="main-action-form">
+        <div>
+          <input
+            type="radio"
+            id="action_upload_progress"
+            name="action_type"
+            value="ActionUploadProgress"
+          />
+          <label> Upload progress, feelings, or blocker for your vision.</label>
+        </div>
+        <br />
 
-                <div>
-                    <input type="radio" id="action_modify_timeline" name="action_type" value="ActionModifyTimeline"/>
-                    <label> Modify a timeline like add milestone of details.</label>
-                </div>
-                <br/>
+        <div>
+          <input
+            type="radio"
+            id="action_modify_timeline"
+            name="action_type"
+            value="ActionModifyTimeline"
+          />
+          <label> Modify a timeline like add milestone of details.</label>
+        </div>
+        <br />
 
-                <div>
-                    <input type="radio" id="action_create_timeline" name="action_type" value="ActionCreateTimeline"/>
-                    <label> Create a new timeline or vision.</label>
-                </div>
+        <div>
+          <input
+            type="radio"
+            id="action_create_timeline"
+            name="action_type"
+            value="ActionCreateTimeline"
+          />
+          <label> Create a new timeline or vision.</label>
+        </div>
 
-                <div className="main-action-form-div-submit-container"><input className="main-action-form-div-input" type="submit" value="Submit"/>
-                </div>
-
-    `
-            </form>
-
+        <div className="main-action-form-div-submit-container">
+          <input
+            className="main-action-form-div-input"
+            type="submit"
+            value="Submit"
+          />
+        </div>
+      </form>
     </main>
-    )
-}; 
+  );
+}
 
 export default Action;
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,35 +1,46 @@
-function Timeline(){
-    return(
-        <main> 
-            <div className="body-div-container">
-        
-            <br/>
-            <br/>
-                <div className="body-div-timeline-container-unit">
-                    <h2>Timeline: Community Fitness Instructor</h2>
-                    <img className="timeline-example-image" src="public/images/outdoor-community-yoga.jpg" alt = "Over 30 people are outside doing a stretch on the grass. The people are wearing work out clothes and are on their knee with hands outstretched turning to the side."/>
-                </div>
-                <br/>  
-                <div className="body-div-timeline-container-unit">
-                    <h2>Timeline: Hike Appalachian Trail</h2>
-                    <img className="timeline-example-image" src="public/images/mount-washington.jpg" alt = "Green hillish mountain top with blue sky, white clouds and blue puddles of water."/>
-                </div>
-                <br/>         
-                <div className="body-div-timeline-container-unit">
-                    <h2>Timeline: Starter Home</h2>
-                    <img className="timeline-example-image" src="public/images/single_family_starter_home_pool_view.jpg" alt="pool side view of a one story brown ranch house"/>
-                    <br/>
-                </div>
-                <br/>
-
-
+function Timeline() {
+  return (
+    <main>
+      <div className="body-div-container">
+        <br />
+        <br />
+        <div className="body-div-timeline-container-unit">
+          <h2>Timeline: Community Fitness Instructor</h2>
+          <img
+            className="timeline-example-image"
+            src="public/images/outdoor-community-yoga.jpg"
+            alt="Over 30 people are outside doing a stretch on the grass. The people are wearing work out clothes and are on their knee with hands outstretched turning to the side."
+          />
         </div>
+        <br />
+        <div className="body-div-timeline-container-unit">
+          <h2>Timeline: Hike Appalachian Trail</h2>
+          <img
+            className="timeline-example-image"
+            src="public/images/mount-washington.jpg"
+            alt="Green hillish mountain top with blue sky, white clouds and blue puddles of water."
+          />
+        </div>
+        <br />
+        <div className="body-div-timeline-container-unit">
+          <h2>Timeline: Starter Home</h2>
+          <img
+            className="timeline-example-image"
+            src="public/images/single_family_starter_home_pool_view.jpg"
+            alt="pool side view of a one story brown ranch house"
+          />
+          <br />
+        </div>
+        <br />
+      </div>
 
-            <section className ="view-action-bottom-buttons">
-                <a className="nav-a-button" href="#" target ="_self">Create New Timeline</a>
-            </section>
-        </main>
-    )
-};
+      <section className="view-action-bottom-buttons">
+        <a className="nav-a-button" href="#" target="_self">
+          Create New Timeline
+        </a>
+      </section>
+    </main>
+  );
+}
 
 export default Timeline;
