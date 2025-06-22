3584c250706b887359235b56acae6c5f74918da5
Melissa Cruz <melissa.e.cruz@gmail.com>
2025-06-22 16:59:28 -0400

    added visuals to the explore

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -11,11 +11,36 @@ function Explore() {
           <h2>Vision: Starter Home</h2>
           <img
             className="timeline-example-image"
-            src="public/images/single_family_starter_home_pool_view.jpg"
+            src="./images/single_family_starter_home_pool_view.jpg"
             alt="pool side view of a one story brown ranch house"
           />
           <p></p>
         </div>
+
+        <div className="body-div-timeline-container-unit">
+          <h2>Vision: Fitness Studio Owner</h2>
+          <img
+            className="timeline-example-image"
+            src="./images/pexels-mart-production-7318661.jpeg"
+            alt="a fitness studio in white"
+          />
+          <p></p>
+        </div>
+
+
+        <div className="body-div-timeline-container-unit">
+          <h2>Vision: Hike Appalachian Trail</h2>
+          <img
+            className="timeline-example-image"
+            src="public/images/mount-washington.jpg"
+            alt="Green hillish mountain top with blue sky, white clouds and blue puddles of water."
+          />
+          <p></p>
+        </div>
+
+
+
+        
         <br />
 
         <div>
